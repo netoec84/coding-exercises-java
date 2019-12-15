aaa4b744725a17f94f902cea828bf4f07001ef13
Jose Guevara <jose@jewif.com>
2019-12-15 12:02:25 -0300

    add OddOccurrencesInArray

---
+++ src/main/java/arrays/OddOccurrencesInArray.java
@@ -0,0 +1,44 @@
+package arrays;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Find value that occurs in odd number of elements.
+ */
+public class OddOccurrencesInArray {
+
+    public int solution(int[] A) {
+        int xor = 0;
+        for (int i = 0; i < A.length; i++) {
+            xor ^= A[i];
+        }
+
+        return xor;
+    }
+
+    @Test
+    public void oneElement() {
+        int[] A = {3};
+        assertEquals(3, new OddOccurrencesInArray().solution(A));
+    }
+
+    @Test
+    public void threeElement() {
+        int[] A = {5, 5, 1};
+        assertEquals(1, new OddOccurrencesInArray().solution(A));
+    }
+
+    @Test
+    public void fiveElements() {
+        int[] A = {5, 5, 3, 7, 7};
+        assertEquals(3, new OddOccurrencesInArray().solution(A));
+    }
+
+    @Test
+    public void nineElements() {
+        int[] A = {9, 3, 9, 3, 9, 7, 9};
+        assertEquals(7, new OddOccurrencesInArray().solution(A));
+    }
+}
